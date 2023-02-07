7b94b9f2f075d7866c52dacb61ce177755711144
Andrey <79315543+Andrey-kl@users.noreply.github.com>
2023-02-07 11:38:52 +0300

    Add files via upload

--- sost/js/option.js
+++ sost/js/option.js
@@ -1,8 +1,35 @@
-// const z1 = 22
 
-// let z1_1 = document.getElementById("z1")
+const t1 = 22
+const t2 = 22
 
-// if (EventTarget.dataset.action === "Buba") {
-//     alert("Huba")
-// }
 
+function end(){
+    let tn1 = document.getElementById("z1").value
+
+    if (tn1 == t1) {
+        document.getElementById("ans1").innerHTML = "Верно"
+    } else if (tn1 == ""){
+        document.getElementById("ans1").innerHTML = "Вы ничего не ввели"
+    } else{
+        document.getElementById("ans1").innerHTML = "Не верно"
+    }
+}
+
+
+
+let time = 14399;
+const countDownEl = document.getElementById("times_all");
+setInterval(updateCountdown, 1000);
+    function updateCountdown() {
+        const minutes = Math.floor(time / 240);
+        const hour = Math.floor(time / 3600);
+        let seconds = time % 60;
+        seconds = seconds < 10 ? "0"+ seconds:seconds;
+        countDownEl.innerHTML = `${hour}:${minutes}:${seconds}`;
+        time--;
+        if (hour == 0 && seconds == 0 && minutes == 0){
+            time++
+            countDownEl.innerHTML = "Время вышло"
+            clearInterval(updateCountdown);
+        }
+}
