f7a5f15867cfaeaf2d08edc6bdc227974aac238e
Andrey <79315543+Andrey-kl@users.noreply.github.com>
2023-02-08 12:01:02 +0300

    Add files via upload

--- sost/js/option.js
+++ sost/js/option.js
@@ -1,17 +1,79 @@
-const t1 = 22
-const t2 = 22
+const e1 = 22
+const e2 = 22
+const e3 = 22
+const e4 = 22
+const e5 = 22
+const e6 = 22
+const e7 = 22
+const e8 = 22
+const e9 = 22
+const e10 = 22
+const e11 = 22
+const e12a = "п/6 + пk, п/3 + пk"
+const e12b = "7п/6; 13п/6; 4п/3; 7п/3"
+const e13 = "arctg 5√2/4"
+const e14 = "(-5; -√22]; [-4; 4]; [√22; 5)"
+const e15 = "9 млн"
+const e16 = 3
+const e17 = "(−∞; -5); (-5; 0); (0; 3); (3; 4)"
+const e18 = 22
+const counter = 0
 
 
 function end(){
-    let tn1 = document.getElementById("z1").value
+    let i1 = document.getElementById("z1").value
+    let i2 = document.getElementById("z1").value
+    let i3 = document.getElementById("z1").value
+    let i4 = document.getElementById("z1").value
+    let i5 = document.getElementById("z1").value
+    let i6 = document.getElementById("z1").value
+    let i7 = document.getElementById("z1").value
+    let i8 = document.getElementById("z1").value
+    let i9 = document.getElementById("z1").value
+    let i10 = document.getElementById("z1").value
+    let i11 = document.getElementById("z1").value
+    let i12 = document.getElementById("z1").value
+    let i13 = document.getElementById("z1").value
+    let i14 = document.getElementById("z1").value
+    let i15 = document.getElementById("z1").value
+    let i16 = document.getElementById("z1").value
+    let i17 = document.getElementById("z1").value
+    let i18 = document.getElementById("z1").value
 
-    if (tn1 == t1) {
-        document.getElementById("ans1").innerHTML = "Верно"
-    } else if (tn1 == ""){
-        document.getElementById("ans1").innerHTML = "Вы ничего не ввели"
-    } else{
-        document.getElementById("ans1").innerHTML = "Не верно"
+    if (i1 == e1) {
+        counter++
     }
+    if (i2 == e4) {
+        counter++
+    }
+    if (i3 == e3) {
+        counter++
+    }
+    if (i4 == e4) {
+        counter++
+    }
+    if (i5 == e5) {
+        counter++
+    }
+    if (i6 == e6) {
+        counter++
+    }
+    if (i7 == e7) {
+        counter++
+    }
+    if (i8 == e8) {
+        counter++
+    }
+    if (i9 == e9) {
+        counter++
+    }
+    if (i10 == e10) {
+        counter++
+    }
+    if (i11 == e11) {
+        counter++
+    }
+       
 }
 
 
