f1f7a56bb0b19338d5e16239fcf76f5a3555098d
Andrey <79315543+Andrey-kl@users.noreply.github.com>
2023-02-08 14:14:00 +0300

    Add files via upload

--- sost/js/loading.js
+++ sost/js/loading.js
@@ -3,26 +3,6 @@ window.onload = function () {
     window.setTimeout(function () {
       document.body.classList.add('loaded');
       document.body.classList.remove('loaded_hiding');
-    }, 2000);
+    }, 1300);
   }
-  $(window).on('load', function () {
-    window.setTimeout(function () {
-      $('.preloader').fadeOut().end().delay(400).fadeOut('slow');
-    }, 2000);
-  });
-
-$(document).on('click', 'a', function(e){
-  $('body').removeClass('loaded');
-  $('body').removeClass('loaded_hiding');
-});
-
-
-$(window).on('load', function () {
-  $('body').removeClass('loaded'); 
-  $('body').addClass('loaded_hiding');
-      
-  window.setTimeout(function () {
-    $('body').addClass('loaded');
-    $('body').removeClass('loaded_hiding');
-  }, 2000);
-});
+  
